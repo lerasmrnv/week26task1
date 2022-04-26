3850aae3018634a07c3d1ca264a84edf28a1ab6e
lerasmrnv <lera.smirnova.flok@gmail.com>
2022-04-26 20:30:51 +0300

    Add Hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Hero from './components/jsx/hero';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+      
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Hero>
+
+        </Hero>
+
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/jsx/hero.jsx
@@ -0,0 +1,13 @@
+import '../styles/normalize.css';
+import '../styles/style.css';
+
+function Hero() {
+    return (
+        <div className='hero-card'>
+            <p>Привет</p>
+
+        </div>
+    );
+}
+
+export default Hero;
