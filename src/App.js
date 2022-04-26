d45f674d14b6846cf34d37de5f38b2f55550502b
lerasmrnv <lera.smirnova.flok@gmail.com>
2022-04-26 21:47:23 +0300

    Added 1 task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import './App.css';
+import './components/styles/normalize.css';
+import './components/styles/style.css';
 import Hero from './components/jsx/hero';
+import heroes from './components/heroes';
 
-// function App() {
-//   return (
-//     <div className="App">
-      
-//     </div>
-//   );
-// }
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <Hero>
-
-        </Hero>
-
+        <h1 className='title'>Галерея крутейших супергероев Marvel</h1>
+        {
+          heroes.map((hero) =>
+          <Hero name={hero.name}  universe={hero.universe} alterEgo={hero.alterEgo} typeOfActivity={hero.typeOfActivity} friends={hero.friends} superpowers={hero.superpowers} image={hero.image} moreDetailed={hero.moreDetailed}></Hero>
+          )
+        }
       </div>
     )
   }
-}
+} 
 
 export default App;
--- src/components/jsx/hero.jsx
+++ src/components/jsx/hero.jsx
@@ -1,11 +1,17 @@
 import '../styles/normalize.css';
 import '../styles/style.css';
 
-function Hero() {
+function Hero(props) {
     return (
         <div className='hero-card'>
-            <p>Привет</p>
-
+            <h2 className='hero-name'>{props.name}</h2>
+            <p className='hero-universe'>Вселенная: {props.universe}</p>
+            <p className='hero-alterEgo'>Альтер эго: {props.alterEgo}</p>
+            <p className='hero-typeOfActivity'>Род деятельности: {props.typeOfActivity}</p>
+            <p className='hero-friends'>Друзья: {props.friends}</p>
+            <p className='hero-superpowers'>Суперсилы: {props.superpowers}</p>
+            <img src={props.image} className='hero-image'></img>
+            <p className='hero-moreDetailed'>{props.moreDetailed}</p>
         </div>
     );
 }
